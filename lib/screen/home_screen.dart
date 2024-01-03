2d21ba89046db19c8ab088e39352145aefb79f12
skdud907 <skdud907@naver.com>
2024-01-03 18:45:19 +0900

    Initial commit

---
+++ lib/screen/home_screen.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.amberAccent,
+        title: Text('Na0'),
+        centerTitle: true,
+      ),
+    );
+  }
+}
